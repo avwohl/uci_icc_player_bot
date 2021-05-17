eba9a7032790a66d5f97ce562bc3f4c8e97328c2
Aaron Wohl <xgit@awohl.com>
2021-05-17 16:54:03 +0000

    .

--- git_update.bash
+++ git_update.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 git pull
-git submodule update --recursive --remote
+# git submodule update --recursive --remote
+git pull --recurse-submodules
 # git pull && git submodule init && git submodule update && git submodule status
